dae304b8b43b3d0726d27bc3e4e7d48618e3047a
rgitrc <114643387+rgitrc@users.noreply.github.com>
2022-10-06 17:23:45 +0530

    Update rulnetcall.m

--- rulnetcall.m
+++ rulnetcall.m
@@ -3,6 +3,7 @@ function out = rulnetcall(unit)
 persistent mynet;
  if isempty(mynet)
      mynet = coder.loadDeepLearningNetwork('rul_net.mat'); % loading the network object
- out = predict(mynet,unit);
  end
+ out = predict(mynet,unit);
+
 end
